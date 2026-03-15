402f3da13fcf7e170165827132c8222e243d6841
Pushyeen <storyofgenji@gmail.com>
2026-03-16 03:34:18 +0800

    現在遊戲開始說明會提醒日文玩家去看攻略

--- js/data/db_scripts.js
+++ js/data/db_scripts.js
@@ -1,13 +1,12 @@
 // js/data/db_scripts.js
 
 export const DB_SCRIPTS = {
-    // 開局創角事件
+// 開局創角事件
     'evt_start_game': [
         { type: "dialog", speaker: "系統", text: "江湖險惡，生死難料。<br>在踏入這片武林之前，先來看看你的資質如何吧。" },
         { type: "roll_stats" },
-        { type: "dialog", speaker: "系統", text: "天命已定！按 WASD 移動。尋找地圖上的【匠】領取裝備，再挑戰【惡】、【徒】、【護】。" }
+        { type: "dialog", speaker: "系統", text: "天命已定！按 WASD 移動。尋找地圖上的【匠】領取裝備，再挑戰【惡】、【徒】、【護】。<br><br><span style='color:#ffff55; font-size:14px;'>※ 日本語のプレイヤーへ：右側の「攻略」タブから日本語ガイドを確認できます。</span>" }
     ],
-
     // 鐵匠給予新手裝備、傷藥與開放商店
     'evt_npc_blacksmith': [
         { type: "dialog", speaker: "好心鐵匠", text: "少俠！前方江湖險惡，讓我來助你一臂之力吧！" },
